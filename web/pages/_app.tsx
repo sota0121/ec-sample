dbc1d5b9bcf2407df9099b4b19aec58b2bf46420
sota_masuda <sota.m777121@gmail.com>
2023-01-15 20:44:21 +0900

    setup _app.tsx for enabling to use mui with next

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import theme from '@/src/theme'
+import createEmotionCache from '@/src/createEmotionCache'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache()
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+export default function App(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
