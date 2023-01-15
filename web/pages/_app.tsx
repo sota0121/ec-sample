38f76d037610cf3ab94e90481501e4d91ecc17c2
sota_masuda <sota.m777121@gmail.com>
2023-01-15 20:58:36 +0900

    setup _document.tsx for enabling to use mui with nextjs

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -19,6 +19,7 @@ export default function App(props: MyAppProps) {
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
       <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
