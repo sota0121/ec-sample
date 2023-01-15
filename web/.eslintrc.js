4230d897b7811bebeedca5df95a10b57079db7e7
sota_masuda <sota.m777121@gmail.com>
2023-01-15 20:33:39 +0900

    setup eslint / prettier / exec those on vscode

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,6 +1,20 @@
 module.exports = {
-  "extends": "next/core-web-vitals",
-  "rules": {
-    "indent": ["error", 2],
-  }
+  extends: [
+    'next/core-web-vitals',
+    'plugin:import/recommended',
+    'plugin:import/warnings',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  rules: {
+    indent: ['error', 2],
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+  },
 }
